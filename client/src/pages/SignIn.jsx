346340b70ece2a3f529f9df42689d826320158e9
goodluckurom <urosoft.me@gmail.com>
2024-07-02 13:44:27 +0100

    authentication

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="min-h-[80vh] flex items-center justify-center  text-text">
+      <div className="w-full max-w-md mx-auto h-[90%] p-6 shadow-md rounded-lg  dark:shadow-sm dark:shadow-accent ">
+        <h1 className="text-3xl text-center font-semibold mb-6 text-accent">
+          Sign In
+        </h1>
+        <form action="" className="flex flex-col gap-4 ">
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="dark:bg-darkBackground dark:text-darkText border-2 border-accent p-3 rounded-lg focus:outline-none "
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="dark:bg-darkBackground dark:text-darkText border-2 border-accent p-3 rounded-lg focus:outline-none "
+          />
+          <button className="mt-4 p-3 bg-accent text-white rounded-lg  transition-colors">
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p className="dark:text-darkText">Don't Have an account?</p>
+          <Link to="/sign-up">
+            <span className="text-accent">Sign Up</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,41 @@
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center  text-text">
+      <div className="w-full max-w-md mx-auto h-[90%] p-6 shadow-md rounded-lg  dark:shadow-sm dark:shadow-accent ">
+        <h1 className="text-3xl text-center font-semibold mb-6 text-accent">
+          Sign Up
+        </h1>
+        <form action="" className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="dark:bg-darkBackground dark:text-darkText border-2 border-accent p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent-light"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="dark:bg-darkBackground dark:text-darkText border-2 border-accent p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent-light"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="dark:bg-darkBackground dark:text-darkText border-2 border-accent p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent-light"
+          />
+          <button className="mt-4 p-3 bg-accent text-white rounded-lg  transition-colors">
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p className="dark:text-darkText">Already have an account?</p>
+          <Link to="/sign-in">
+            <span className="text-accent">Sign In</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
